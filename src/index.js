d00287e8c8abe78e593235475c9e1004b32ec036
zari-nabi <m.nabi.zahra@gmail.com>
2021-04-21 11:06:13 +0430

    first commit news

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
+
+import './index.css';
+import App from './App';
+import bootstrap from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+    , document.getElementById('root'));
